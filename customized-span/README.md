e66c628a34d8afdc705850f8f9bcb13a3d48ba94
haidv7 <haidev.285@gmail.com>
2021-09-03 11:46:50 +0700

    change: update customized example

--- customized-span/README.md
+++ customized-span/README.md
@@ -28,62 +28,12 @@ then wait a sec to pull the images
 1. Step 1: Run the following command to make a call to `order-service`
 
 ```bash
-curl -X GET http://localhost:$ORDER_SERVICE_POST/orders/1
+curl --location --request POST 'localhost:3001/orders' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+    "name": "Hello"
+}'
 ```
 
 2. Step 2: Go to the Zipkin dashboard at http://localhost:${ZIPKIN_PORT}/zipkin, then query to visualize your trace
 ![Trace](./visualize-trace.png)
-
-## How it work
-
-### Creating your own span
-
-Below is the example of creating custom span.
-
-```js
-  app.get('/orders/:id', async (req, res) => {
-  const orderId = req.params.id;
-
-  // start span for HTTP Request
-  const span = tracer.startSpan(`GET /shipments`, {
-    attributes: {
-      [SemanticAttributes.HTTP_METHOD]: "GET",
-      [SemanticAttributes.HTTP_FLAVOR]: "1.1",
-      [SemanticAttributes.HTTP_URL]: "http://shipment-service:3002/shipments",
-    },
-    kind: api.SpanKind.SERVER
-  });
-
-  span.setAttribute("order.id", orderId);
-
-
-  try {
-    const { data: shipments } = await axios.get(`http://shipment-service:3002/shipments?orderId=${orderId}`);
-
-    if (shipments) {
-      span.setAttribute("http.status_code", 200);
-      // update status of span when the work is done
-      span.setStatus({ code: api.SpanStatusCode.OK });
-
-      span.end()
-
-      res.json({
-        id: orderId,
-        type: 'order',
-        attributes: {
-          shipments,
-        }
-      });
-    }
-  } catch (err) {
-    console.log(err);
-
-    // recordException converts the error into a span event.
-    span.recordException(err);
-    // update status of span to error when it encounters an error
-    span.setStatus({ code: api.SpanStatusCode.ERROR });
-    return res.json({
-      error: 'Internal Server Error'
-    });
-  }
-})
--- customized-span/order-service/app.js
+++ customized-span/order-service/app.js
@@ -1,7 +1,8 @@
 
-const axios = require('axios');
 const api = require('@opentelemetry/api');
 const {  SemanticAttributes } = require('@opentelemetry/semantic-conventions');
+const bodyParser = require('body-parser')
+const { MongoClient } = require('mongodb');
 
 const initTracer = require('./tracer');
 
@@ -11,48 +12,75 @@ const express = require('express');
 const app = express();
 const port = process.env.ORDER_SERVICE_PORT || 3001;
 
+app.use(bodyParser.json());
 
-app.get('/orders/:id', async (req, res) => {
-  const orderId = req.params.id;
+const url = 'mongodb://database:27017';
+const client = new MongoClient(url);
 
-  // start span for HTTP Request
-  const span = tracer.startSpan(`GET /shipments`, {
+// Database Name
+const dbName = 'customized-span';
+
+async function createOrder(orderData) {
+  const span = tracer.startSpan(`DB.CREATE_DOCUMENT`, {
     attributes: {
-      [SemanticAttributes.HTTP_METHOD]: "GET",
-      [SemanticAttributes.HTTP_FLAVOR]: "1.1",
-      [SemanticAttributes.HTTP_URL]: `${process.env.SHIPMENT_SERVICE_URL}/shipments`,
+      [SemanticAttributes.DB_MONGODB_COLLECTION]: 'orders',
+      [SemanticAttributes.DB_CONNECTION_STRING]: `url/${dbName}`,
+      document: orderData
     },
     kind: api.SpanKind.SERVER
   });
 
-  span.setAttribute("order.id", orderId);
+  try {
+    await client.connect();
 
+    const db = client.db(dbName);
+    const collection = db.collection('orders');
+
+    await collection.insertOne(orderData);
+
+    span.setStatus({ code: api.SpanStatusCode.OK });
+
+    span.end();
+
+    return orderData;
+  } catch (err) {
+    span.setStatus({ code: api.SpanStatusCode.ERROR });
+
+     throw err;
+  }
+}
+
+
+app.post('/orders', async (req, res) => {
+  // start span for HTTP Request
+  const createOrderSpan = tracer.startSpan(`HANDLER.CREATE_ORDER`, {
+    attributes: {},
+    kind: api.SpanKind.SERVER
+  });
 
   try {
-    const { data: shipments } = await axios.get(`${process.env.SHIPMENT_SERVICE_URL}/shipments?orderId=${orderId}`);
+    // Create a new context from the current context which has the span "active"
+    const ctx = api.trace.setSpan(api.context.active(), createOrderSpan);
 
-    if (shipments) {
-      span.setAttribute("http.status_code", 200);
-      // update status of span
-      span.setStatus({ code: api.SpanStatusCode.OK });
+    // calling createOrder function with context whose parentSpan is createOrderSpan
+    let order;
+    api.context.with(ctx, async () => {
+      order = await createOrder(req.body)
+    });
 
-      span.end()
 
-      res.json({
-        id: orderId,
-        type: 'order',
-        attributes: {
-          shipments,
-        }
-      });
-    }
+    createOrderSpan.setStatus({ code: api.SpanStatusCode.OK });
+    createOrderSpan.end();
+
+    return res.json({
+      data: order,
+    });
   } catch (err) {
     console.log(err);
-
     // recordException converts the error into a span event.
-    span.recordException(err);
+    createOrderSpan.recordException(err);
     // update status of span
-    span.setStatus({ code: api.SpanStatusCode.ERROR });
+    createOrderSpan.setStatus({ code: api.SpanStatusCode.ERROR });
     return res.json({
       error: 'Internal Server Error'
     });
